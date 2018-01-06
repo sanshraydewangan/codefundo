e0ddab808dddf4a63bd2263904c3b0c15c521ce6
Venu Gopal Gajam <Venu Gopal Gajam>
2018-01-06 15:06:51 +0530

    added /trainenquiry

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var 
 var fs = require('fs');
 
 app.set('view engine', 'ejs');
@@ -11,6 +12,10 @@ app.get('/', function(req, res) {
 	res.render('index.ejs');
 });
 
+app.get('/trainenquiry',function(req,res){
+	res.write("data received!!");
+	res.end();
+})
 app.listen(8000, function() {
 	console.log('listening on port 8000');
 });
